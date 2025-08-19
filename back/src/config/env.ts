751fb0e7f19b807769988e07d0b5499c48ab3a26
florludovico <florencia.ludovico@gmail.com>
2025-08-19 16:40:33 -0300

    fix env.ts

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -12,4 +12,4 @@ export const DB_SYNC: boolean = process.env.DB_SYNC ? process.env.DB_SYNC === "t
 export const DB_LOGGING: boolean = process.env.DB_LOGGING ? process.env.DB_LOGGING === "true" : true
 export const DB_ENTITIES: string[] = process.env.DB_ENTITIES ? process.env.DB_ENTITIES.split(","): ["src/entities/**/*.ts"]
 export const DB_DROP: boolean = process.env.DB_DROP ? process.env.DB_DROP === "true" : true
-export const DB_SSL = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+export const DB_SSL = { rejectUnauthorized: false};
