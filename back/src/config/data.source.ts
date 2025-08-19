6ee364d96627fee92a14d5fdbd6a9b48f800a0dc
florludovico <florencia.ludovico@gmail.com>
2025-08-19 17:00:22 -0300

    fix data.source and env.ts

--- back/src/config/data.source.ts
+++ back/src/config/data.source.ts
@@ -1,15 +1,11 @@
 import { DataSource, Repository } from "typeorm";
-import {DB_DATABASE, DB_DROP, DB_ENTITIES, DB_HOST, DB_LOGGING, DB_PASSWORD, DB_PORT, DB_SSL, DB_SYNC, DB_TYPE, DB_USERNAME,} from "./env";
+import {DATABASE_URL, DB_DROP, DB_ENTITIES,  DB_LOGGING, DB_SSL, DB_SYNC, } from "./env";
 import { User } from "../entities/User.entity";
 import { Credential } from "../entities/Credentials.entity";
 
 export const AppDataSource = new DataSource({
-  type: DB_TYPE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  type: "postgres",
+  url: DATABASE_URL,
   synchronize: DB_SYNC,
   logging: DB_LOGGING,
   entities: DB_ENTITIES,
